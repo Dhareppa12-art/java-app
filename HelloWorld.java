9e38df77cad0f255d6eb6e384edecfd66b59100c
EC2 Default User <ec2-user@ip-172-31-8-243.ap-south-1.compute.internal>
2026-09-05 17:55:43 +0000

    Update HelloWorld message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from AWS EC2!");
+        System.out.println("Hello from AWS EC2 - Version 2!");
     }
 }
